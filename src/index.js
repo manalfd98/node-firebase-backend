f282bb3ab45b71f0c9f2c6371c6cb9b42d1b7722
manaldf98 <manalfd98@gmail.com>
2024-07-10 21:57:09 +0500

    auth test added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const router = express.Router();
 
 // Load environment variables from .env file
 dotenv.config();
@@ -7,9 +8,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Define the /auth route
+router.get('/auth', (req, res) => {
+  res.send('auth successful');
 });
+
+app.use("/",router)
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
