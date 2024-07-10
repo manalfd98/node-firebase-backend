637d48d55a95393dd10b6176f93dee8c39b4cb59
manaldf98 <manalfd98@gmail.com>
2024-07-10 21:26:28 +0500

    intialized

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
